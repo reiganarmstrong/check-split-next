b372a303eb40b90ebadc6d4952d346c0be6f04e4
Reagan Armstrong <reagancstr@gmail.com>
2025-10-26 09:27:54 -0400

    Tailwind cleanup.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import AuthWrapper from "./_components/auth-wrapper";
 import NavBar from "./_components/navbar";
 
 export default function RootLayout({
@@ -8,10 +9,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="h-[100vh]">
+      <body className="h-screen">
         <div className="pl-10 pr-10 pb-15 font-serrif text-primary h-full min-h-[820px] flex flex-col">
           <NavBar />
-          {children}
+          <AuthWrapper>{children}</AuthWrapper>
         </div>
       </body>
     </html>
